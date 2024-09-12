crdroidandroid/android_device_oneplus_sm8250-common

sm8250-common: oplus cam: update camera flag

BoardConfigCommon.mk
@@ -60,7 +60,7 @@ TARGET_BOOTLOADER_BOARD_NAME := kona
 
 # Camera
 TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED := true
-TARGET_USES_OPLUS_CAMERA := true
+TARGET_CAMERA_PACKAGE_NAME := com.oplus.camera
 
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop

Broly1/device_oneplus_sm8250-common

BoardConfigCommon.mk
@@ -59,7 +59,7 @@ TARGET_BOOTLOADER_BOARD_NAME := kona
 
 # Camera
 TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED := true
-TARGET_USES_OPLUS_CAMERA := true
+TARGET_CAMERA_PACKAGE_NAME := com.oplus.packageName
 
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop